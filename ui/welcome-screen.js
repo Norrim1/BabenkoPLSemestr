11ac48c8726e3ace3a918d8e09d8aea8cd1f73e2
Norrim1 <danilbabenko76840@gmail.com>
2025-06-04 15:25:51 +0700

    Welcome screen function
    Welcome screen functional completion

---
+++ ui/welcome-screen.js
@@ -0,0 +1,28 @@
+let nextButton = document.getElementById("next-button");
+let nameInput = document.getElementById("welcome-name-input");
+
+nameInput.addEventListener('input', checkName);
+nameInput.addEventListener('change', checkName);
+
+nextButton.disabled = true;
+
+function validName(name) {
+    const regex = /^[a-zA-Zа-яА-ЯёЁ]+$/;
+    return regex.test(name);
+}
+
+function checkName() {
+    let nameFilledCorrectly = true;
+    if (nameInput.value.trim() === '') { 
+        nameFilledCorrectly = false; 
+    } 
+    nextButton.disabled = !nameFilledCorrectly;
+}
+
+nextButton.addEventListener('click', () => {
+    if (!validName(nameInput.value)) {
+        alert('Имя не должно содержать цифр, пробелов или специальных символов.');
+        return;
+    }
+    document.getElementById("welcome-screen").style.display = 'none';
+});
